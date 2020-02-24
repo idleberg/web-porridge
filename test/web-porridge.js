579e235005bce21119b23240a95928e5148ccdc7
idleberg <git@idleberg.com>
2020-02-24 22:32:20 +0100

    rename tests

--- test/web-porridge.js
+++ test/web-porridge.js
@@ -16,7 +16,7 @@ const actualBase64Object = encode(actualObject);
 const invalidBase64JSON = encode(invalidJSON);
 
 
-test(`getItem() String`, t => {
+test(`getItem(): String`, t => {
 	localStorage.setItem('demo', actualString);
 
   const expected = localPorridge.getItem('demo');
@@ -24,7 +24,7 @@ test(`getItem() String`, t => {
 	t.is(actualString, expected);
 });
 
-test(`getItem() Valid JSON`, t => {
+test(`getItem(): Valid JSON`, t => {
 	localStorage.setItem('demo', JSON.stringify(actualObject));
 
   const actual = localPorridge.getItem('demo');
@@ -32,7 +32,7 @@ test(`getItem() Valid JSON`, t => {
 	t.deepEqual(actualObject, actual);
 });
 
-test(`getItem() Invalid JSON`, t => {
+test(`getItem(): Invalid JSON`, t => {
 	localStorage.setItem('demo', invalidJSON);
 
   const actual = localPorridge.getItem('demo');
@@ -40,7 +40,7 @@ test(`getItem() Invalid JSON`, t => {
 	t.is(invalidJSON, actual);
 });
 
-test(`getItem() true`, t => {
+test(`getItem(): true`, t => {
 	localStorage.setItem('demo', 'true');
 
   const expected = true;
@@ -49,7 +49,7 @@ test(`getItem() true`, t => {
 	t.is(actual, expected);
 });
 
-test(`getItem() false`, t => {
+test(`getItem(): false`, t => {
 	localStorage.setItem('demo', 'false');
 
   const expected = false;
@@ -58,7 +58,7 @@ test(`getItem() false`, t => {
 	t.is(actual, expected);
 });
 
-test(`getItem() Int`, t => {
+test(`getItem(): Int`, t => {
 	localStorage.setItem('demo', '1');
 
   const expected = 1;
@@ -67,7 +67,7 @@ test(`getItem() Int`, t => {
 	t.is(actual, expected);
 });
 
-test(`getItem() Float`, t => {
+test(`getItem(): Float`, t => {
 	localStorage.setItem('demo', '1.2');
 
   const expected = 1.2;
@@ -76,7 +76,7 @@ test(`getItem() Float`, t => {
 	t.is(actual, expected);
 });
 
-test(`getItem() null`, t => {
+test(`getItem(): null`, t => {
 	localStorage.setItem('demo', 'null');
 
   const expected = null;
@@ -85,7 +85,7 @@ test(`getItem() null`, t => {
 	t.is(actual, expected);
 });
 
-test(`getItem() Base64 String`, t => {
+test(`getItem(): Base64 String`, t => {
 	localStorage.setItem('demo', actualBase64String);
 
   const actual = localPorridge.getItem('demo');
@@ -93,7 +93,7 @@ test(`getItem() Base64 String`, t => {
 	t.is(actualString, actualString);
 });
 
-test(`getItem() Base64 Valid JSON`, t => {
+test(`getItem(): Base64 Valid JSON`, t => {
 	localStorage.setItem('demo', actualBase64Object);
 
   const actual = localPorridge.getItem('demo');
@@ -101,7 +101,7 @@ test(`getItem() Base64 Valid JSON`, t => {
 	t.deepEqual(actualObject, actual);
 });
 
-test(`getItem() Base64 Invalid JSON`, t => {
+test(`getItem(): Base64 Invalid JSON`, t => {
 	localStorage.setItem('demo', invalidBase64JSON);
 
   const actual = localPorridge.getItem('demo');
@@ -109,7 +109,7 @@ test(`getItem() Base64 Invalid JSON`, t => {
 	t.is(invalidJSON, actual);
 });
 
-test(`getItem() Object key`, t => {
+test(`getItem(): Object key`, t => {
 
 	localStorage.setItem('demo', JSON.stringify(actualObject));
 
@@ -118,7 +118,7 @@ test(`getItem() Object key`, t => {
 	t.is(actualString, expected);
 });
 
-test(`setItem() String`, t => {
+test(`setItem(): String`, t => {
 	localPorridge.setItem('demo', actualString);
 
   const expected = localStorage.getItem('demo');
@@ -126,7 +126,7 @@ test(`setItem() String`, t => {
 	t.is(actualString, expected);
 });
 
-test(`setItem() JSON`, t => {
+test(`setItem(): JSON`, t => {
 	localPorridge.setItem('demo', actualObject);
 
   const expected = JSON.parse(localStorage.getItem('demo'));
@@ -134,7 +134,7 @@ test(`setItem() JSON`, t => {
 	t.deepEqual(actualObject, expected);
 });
 
-test(`setItem() Object key`, t => {
+test(`setItem(): Object key`, t => {
 	localPorridge.setItem('demo', actualString, 'nested');
 
   const expected = JSON.parse(localStorage.getItem('demo')).nested;
@@ -142,7 +142,7 @@ test(`setItem() Object key`, t => {
 	t.deepEqual(actualString, expected);
 });
 
-test(`setItem() true`, t => {
+test(`setItem(): true`, t => {
 	localPorridge.setItem('demo', true);
 
   const expected = localStorage.getItem('demo');
@@ -150,7 +150,7 @@ test(`setItem() true`, t => {
 	t.is('true', expected);
 });
 
-test(`setItem() false`, t => {
+test(`setItem(): false`, t => {
 	localPorridge.setItem('demo', false);
 
   const expected = localStorage.getItem('demo');
@@ -158,7 +158,7 @@ test(`setItem() false`, t => {
 	t.is('false', expected);
 });
 
-test(`setItem() null`, t => {
+test(`setItem(): null`, t => {
 	localPorridge.setItem('demo', null);
 
   const expected = localStorage.getItem('demo');
@@ -166,7 +166,7 @@ test(`setItem() null`, t => {
 	t.is('null', expected);
 });
 
-test(`setItem() Int`, t => {
+test(`setItem(): Int`, t => {
 	localPorridge.setItem('demo', 1);
 
   const expected = localStorage.getItem('demo');
@@ -174,7 +174,7 @@ test(`setItem() Int`, t => {
 	t.is('1', expected);
 });
 
-test(`setItem() Float`, t => {
+test(`setItem(): Float`, t => {
 	localPorridge.setItem('demo', 1.2);
 
   const expected = localStorage.getItem('demo');
@@ -182,7 +182,7 @@ test(`setItem() Float`, t => {
 	t.is('1.2', expected);
 });
 
-test(`removeItem() String`, t => {
+test(`removeItem(): String`, t => {
 	localStorage.setItem('demo', actualString);
   localPorridge.removeItem('demo');
 
@@ -192,7 +192,7 @@ test(`removeItem() String`, t => {
 	t.is(actual, expected);
 });
 
-test(`removeItem() Object key`, t => {
+test(`removeItem(): Object key`, t => {
 	localStorage.setItem('demo', JSON.stringify({
     ...actualObject,
     deleteMe: true
@@ -214,7 +214,7 @@ test(`decode() String`, t => {
   t.is(decodedStorage, decodedPorridge)
 });
 
-test(`decode() Valid JSON`, t => {
+test(`Base64: Valid JSON`, t => {
   localStorage.setItem('demo', encode(JSON.stringify(actualObject)));
 
   const decodedStorage = JSON.parse(decode(localStorage.getItem('demo')));
@@ -223,7 +223,7 @@ test(`decode() Valid JSON`, t => {
   t.deepEqual(decodedStorage, decodedPorridge)
 });
 
-test(`decode() Invalid JSON`, t => {
+test(`Base64: Invalid JSON`, t => {
   localStorage.setItem('demo', encode(invalidJSON));
 
   const decodedStorage = decode(localStorage.getItem('demo'));
@@ -232,7 +232,7 @@ test(`decode() Invalid JSON`, t => {
   t.deepEqual(decodedStorage, decodedPorridge)
 });
 
-test(`decode() true`, t => {
+test(`Base64: true`, t => {
   localStorage.setItem('demo', encode(true));
 
   const decodedStorage = JSON.parse(decode(localStorage.getItem('demo')));
@@ -241,7 +241,7 @@ test(`decode() true`, t => {
   t.is(decodedStorage, decodedPorridge)
 });
 
-test(`decode() false`, t => {
+test(`Base64: false`, t => {
   localStorage.setItem('demo', encode(false));
 
   const decodedStorage = JSON.parse(decode(localStorage.getItem('demo')));
@@ -250,7 +250,7 @@ test(`decode() false`, t => {
   t.is(decodedStorage, decodedPorridge)
 });
 
-test(`decode() null`, t => {
+test(`Base64: null`, t => {
   localStorage.setItem('demo', encode(null));
 
   const decodedStorage = JSON.parse(decode(localStorage.getItem('demo')));
@@ -259,7 +259,7 @@ test(`decode() null`, t => {
   t.is(decodedStorage, decodedPorridge)
 });
 
-test(`decode() Int`, t => {
+test(`Base64: Int`, t => {
   localStorage.setItem('demo', encode(1));
 
   const decodedStorage = JSON.parse(decode(localStorage.getItem('demo')));
@@ -268,7 +268,7 @@ test(`decode() Int`, t => {
   t.is(decodedStorage, decodedPorridge)
 });
 
-test(`decode() Float`, t => {
+test(`Base64: Float`, t => {
   localStorage.setItem('demo', encode(1.2));
 
   const decodedStorage = JSON.parse(decode(localStorage.getItem('demo')));
