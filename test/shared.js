9fe5752ca53a4cb29e757a0893432a016cab8e33
idleberg <git@idleberg.com>
2020-03-05 00:02:23 +0100

    change exports

--- test/shared.js
+++ test/shared.js
@@ -1,14 +1,13 @@
-const { localPorridge } = require('../lib');
-const { encode } = require('../lib/base64');
+const { localPorridge, base64Encode } = require('../lib');
 
 const actualString = 'Hello World!';
 const actualObject = {
   nested: 'Bye World!'
 };
 const invalidJSON = '{"nested":"Hello World!"';
-const actualBase64String = encode(actualString);
-const actualBase64Object = encode(actualObject);
-const invalidBase64JSON = encode(invalidJSON);
+const actualBase64String = base64Encode(actualString);
+const actualBase64Object = base64Encode(actualObject);
+const invalidBase64JSON = base64Encode(invalidJSON);
 
 module.exports = {
   actualString: actualString,
