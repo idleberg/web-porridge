47eae7c5a52e348be87dfd7195a72f99bdd30c21
idleberg <git@idleberg.com>
2020-03-31 19:14:45 +0200

    build without comments

--- lib/porridge-db.d.ts
+++ lib/porridge-db.d.ts
@@ -3,83 +3,17 @@ export default class WebPorridgeDB {
     store: any;
     options: WebPorridgeOptions;
     constructor(userOptions?: WebPorridgeOptions);
-    /**
-     * Reads single data item from WebStorage type
-     * @param {String} item
-     * @param {Object} subKeyName
-     * @returns {*}
-     */
     getItem(keyName: string, subKeyName?: string | null, options?: WebPorridgeOptions): any;
-    /**
-  * Writes data items to WebStorage type
-  * @param {Array} item
-  * @returns {*}
-  */
     getItems(input: (string | PayloadOptions)[], options?: WebPorridgeOptions): Promise<any[]>;
-    /**
-     * Removes single data item from WebStorage type
-     * @param {String} item
-     * @param {Object} subKeyName
-     */
     removeItem(keyName: string, subKeyName?: string): Promise<any>;
-    /**
-     * Removes datas item from WebStorage type
-     * @param {String} input
-     */
     removeItems(input: (string | PayloadOptions)[]): Promise<any[]>;
-    /**
-    * Writes single data item to WebStorage type
-    * @param {String} item
-    * @param {*} value
-    * @param {Object} userOptions
-    * @returns {*}
-    */
     setItem(keyName: string, keyValue: any, subKeyName?: string): any;
-    /**
-    * Writes data items to WebStorage type
-    * @param {Array} item
-    * @returns {*}
-    */
     setItems(input: PayloadOptions[]): Promise<any[]>;
-    /**
-     * Returns the length of WebStorage type
-     * @param {Integer} index
-     * @returns {*}
-     */
     key(index: number): Promise<IDBValidKey>;
-    /**
-     * Returns the length of WebStorage type
-     * @returns {Integer}
-     */
     get length(): Promise<number>;
-    /**
-     * Clears WebStorage type
-     * @returns {*}
-     */
     clear(): Promise<void>;
-    /**
-     * Registers an event listener on the window or custom element
-     * @param {Element|Window} element
-     * @returns {*}
-     */
     listen(element?: Element | Window): void;
-    /**
-     * Removes an event listener on the window or custom element
-     * @param {Element|Window} element
-     * @returns {*}
-     */
     mute(element?: Element | Window): void;
-    /**
-     * Dispatches an event to WebPorridge listeners
-     * @param {String} action
-     * @param {*} payload
-     * @returns {*}
-     */
     dispatch(action: string, payload: Number | PayloadOptions): void;
-    /**
-     * Event handler
-     * @param {Event} event
-     * @returns {void}
-     */
     private eventHandler;
 }
--- lib/util.d.ts
+++ lib/util.d.ts
@@ -1,56 +1,10 @@
-/**
- * Checks whether input data requires deserialization after reading it from WebStorage
- * @param {*} inputData
- * @returns {boolean}
- */
 declare function maybeDeserialize(inputData: any): boolean;
-/**
- * Checks whether input data requires serialization prior to writing it to WebStorage
- * @param {*} inputString
- * @returns {boolean}
- */
 declare function maybeSerialize(inputString: string | Object): boolean;
-/**
- * Base64-decodes input data if necessary. Supports deserialization
- * @param {string} inputString
- * @param {object} options
- * @returns {string|Object}
- */
 declare function maybeBase64Decode(inputString: string, options?: WebPorridgeOptions): any;
-/**
- * Base64-encodes input string. Supports serialization
- * @param {*} inputString
- * @returns {string}
- */
 declare function base64Encode(inputString: string): string;
-/**
- * Base64-decodes input string
- * @param {*} inputString
- * @returns {string}
- */
 declare function base64Decode(inputString: string): string;
-/**
- * Checks for supported WebStorage methods
- * @param {string} action
- * @returns {boolean}
- */
 declare function validateAction(action: string): void;
-/**
- * Detect whether input is of type Array
- * @param {*} inputData
- * @returns {boolean}
- */
 declare function isArray(inputData: any): boolean;
-/**
- * Detect whether input is of type Object
- * @param {*} inputData
- * @returns {boolean}
- */
 declare function isObject(inputData: any): boolean;
-/**
- * Determines whether a floating-point number can be safely serialized
- * @param {*} inputData
- * @returns {boolean}
- */
 declare function isSerializableNumber(inputData: any): boolean;
 export { base64Decode, base64Encode, isArray, isObject, isSerializableNumber, maybeBase64Decode, maybeDeserialize, maybeSerialize, validateAction };
