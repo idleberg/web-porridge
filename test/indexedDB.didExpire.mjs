5f41013f9a82be8c29d6a01e2ff9bbfae11ff991
Jan T. Sott <git@idleberg.com>
2022-04-09 18:39:53 +0200

    add missing test

---
+++ test/indexedDB.didExpire.mjs
@@ -0,0 +1,34 @@
+import 'fake-indexeddb/auto.js';
+import { Navigator } from 'node-navigator';
+import { values } from './shared.mjs';
+import { PorridgeDB } from '../lib/web-porridge.mjs';
+import browserEnv from 'browser-env';
+import test from 'ava';
+
+browserEnv(['window']);
+global['navigator'] = new Navigator();
+window.indexedDB = {}
+
+const db = new PorridgeDB();
+
+test.beforeEach(async () => {
+	await db.clear();
+});
+
+test.serial('Did expire', async t => {
+  await db.setItem('demo', values.string, { expires: Date.now() - 1000});
+
+  const actual = await db.didExpire('demo');
+  const expected = true;
+
+  t.is(actual, expected);
+});
+
+test.serial('Did not expire)', async t => {
+  await db.setItem('demo', values.string, { expires: Date.now() + 1000});
+
+  const actual = await db.didExpire('demo');
+  const expected = false;
+
+  t.is(actual, expected);
+});
