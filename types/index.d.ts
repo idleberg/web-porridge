38c21f7ea50cd61599195b8895009cb842678b67
Jan T. Sott <git@idleberg.com>
2023-09-09 16:42:42 +0200

    use types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,21 +1,21 @@
 declare namespace WebPorridge {
-	interface StorageOptions{
+	type StorageOptions ={
 		expires?: string;
 		prop?: string;
 	}
 
-	interface IndexeddbOptions {
+	type IndexeddbOptions = {
 		db: string;
 		name: string;
 	}
 
-	interface Payload {
+	type Payload = {
 		'@expires'?: string;
 		'@type': 'boolean' | 'date' | 'null' | 'number' | 'object' | 'string' | 'undefined';
 		'@value': string;
 	}
 
-	interface StorageKeys {
+	type StorageKeys = {
 		expires: '@expires';
 		value: '@value';
 		type: '@type';
