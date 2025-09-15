534f322b285aafb0e3231f9baac7b80bc297e23c
Jan T. Sott <git@idleberg.com>
2025-09-15 17:05:39 +0200

    chore: switch to tsdown as bundler

---
+++ tsdown.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig((options) => {
+	const isProduction = options.watch !== true;
+
+	return {
+		target: "node20",
+		clean: isProduction,
+		dts: isProduction,
+		entry: ["src/index.ts"],
+		format: "esm",
+		minify: isProduction,
+		outDir: "lib",
+	};
+});
