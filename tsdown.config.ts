3b37819f91fda7a1e5bb2680e202455dfbdda61d
Jan T. Sott <git@idleberg.com>
2025-09-15 17:07:07 +0200

    chore: format

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "tsdown";
+import { defineConfig } from 'tsdown';
 
 export default defineConfig((options) => {
 	const isProduction = options.watch !== true;
 
 	return {
-		target: "node20",
+		target: 'node20',
 		clean: isProduction,
 		dts: isProduction,
-		entry: ["src/index.ts"],
-		format: "esm",
+		entry: ['src/index.ts'],
+		format: 'esm',
 		minify: isProduction,
-		outDir: "lib",
+		outDir: 'lib',
 	};
 });
